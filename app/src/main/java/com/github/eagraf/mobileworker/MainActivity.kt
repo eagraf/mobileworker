6ef16cfd8f601bad5992a30d82f3bd9ace762c56
ethan <graf.ethan@gmail.com>
2020-04-16 13:17:55 -0400

    Connection toggle button

--- app/src/main/java/com/github/eagraf/mobileworker/MainActivity.kt
+++ app/src/main/java/com/github/eagraf/mobileworker/MainActivity.kt
@@ -2,11 +2,27 @@ package com.github.eagraf.mobileworker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ToggleButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val connectionToggle: ToggleButton = findViewById(R.id.connectionToggle)
+        connectionToggle.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // Attempt to connect to synchronizer
+                Log.d("MainActivity", "Connect")
+
+            } else {
+                // Disconnect from synchronizer
+                Log.d("MainActivity", "Disconnect")
+
+            }
+        }
     }
 }
